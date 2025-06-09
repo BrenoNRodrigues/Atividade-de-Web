e243f99a646ee29a7e313ab09c4a7061cb36be8c
BrenoNRodrigues <breno.rodrigues.nascimento.silva@gmail.com>
2025-06-09 14:42:51 -0300

    Update pagecontato.js

--- Template/js/componentes/pagecontato.js
+++ Template/js/componentes/pagecontato.js
@@ -8,6 +8,7 @@ export default function contato(){
     <div>
      <h2 class="section-title">Fale Conosco</h2>
       <p>Estamos localizados na Rua Parintins, 400, Jardim Helena, São Paulo - SP.</p><div><strong>Whatsapp:</strong> (11) 9 6624-0118</div>
+    <div id="map"></div>
     </section>
     </div>
   </div>
