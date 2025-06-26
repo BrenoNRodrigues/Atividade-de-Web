4409367003d658934ebd20ecab6a4da2851b2c3b
BrenoNRodrigues <breno.rodrigues.nascimento.silva@gmail.com>
2025-06-26 04:06:40 -0300

    All-in
    One

---
+++ Atividade-de-Web-Breno/Template/js/componentes/navbar.js
@@ -0,0 +1,23 @@
+export default function navbar (){
+    const nav = document.getElementById("navbar")
+nav.innerHTML = ` <nav class="greeneco" role="navigation">
+<div class="nav-wrapper container">
+  <a id="logo-container" href="/Template/EcoTubosLogotipo.png" class="brand-logo"><img src="/Template/EcoTubosLogotipo.png" alt="EcoTubosLogotipo"></a>
+  <ul class="right hide-on-med-and-down">
+    <li><a href="#home">Produtos</a></li>
+    <li><a href="#servicos">Serviços</a></li>
+    <li><a href="#contato">Contato</a></li>
+    <li><a href="#sobrenos">Sobre Nos</a><li>
+  </ul>
+
+  <ul id="nav-mobile" class="sidenav">
+    <li><a href="#index">Produtos</a></li>
+    <li><a href="#servicos">Serviços</a></li>
+    <li><a href="#contato">Contato</a></li>
+    <li><a href="#sobrenos">Sobre Nos</a></li>
+  </ul>
+  <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+</div>
+</nav>
+`
+}
---
+++ Atividade-de-Web-Breno/Template/js/main.js
@@ -0,0 +1,29 @@
+import navbar from './componentes/navbar.js';
+import home from './componentes/pagehome.js';
+import servicos from './componentes/pageservicos.js'
+import contato from './componentes/pagecontato.js'
+import footer from './componentes/footer.js'
+import sobrenos from './componentes/sobrenos.js';
+
+navbar()
+home()
+footer()
+
+window.addEventListener("hashchange", ()=> {
+    switch(location.hash){
+        case "#home":
+            home()
+        break
+        case "#servicos":
+            servicos()
+        break
+        case "#contato":
+            contato()
+        break
+        case "#sobrenos":
+            sobrenos()
+        break
+
+        
+    }
+})
