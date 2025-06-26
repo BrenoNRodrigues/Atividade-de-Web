d96ecd5447d61b1715871a62386403ebfbe9cc8f
BrenoNRodrigues <breno.rodrigues.nascimento.silva@gmail.com>
2025-06-26 16:26:57 -0300

    sos

---
+++ js/componentes/footer.js
@@ -0,0 +1,22 @@
+export default function footer (){
+    const footer = document.getElementById("foter")
+    foter.innerHTML = `
+   <footer class="page-footer green darken-2">
+    <div class="container">
+      <div class="row">
+        <div class="col s12 m6 footer-email">
+          <h4>Emails</h4> <img src="/Gmail_icon_(2020).svg.png" alt="">
+          <p class="p-footer">vendas.ecotubos001@gmail.com</p>
+          <p class="p-footer">vendas.ecotubos002@gmail.com</p>
+          <p class="p-footer">compras.ecotubos01@gmail.com</p>
+        </div>
+        <div class="col s12 m6 footer-contact">
+          <h4>Contatos</h4>  <img src="/WhatsApp.svg.webp" alt="">
+    <img src="/instagram.png" alt="">
+          <p class="p-footer">Fábio Pirozeli: (11) 9 6624-0118</p>
+          <p class="p-footer">Paula: (11) 9 1640-0779</p>
+          <p class="p-footer">Sheila: (11) 9 7352-9553</p>
+          <p class="p-footer">Mariana: (11) 9 7596-0393</p>
+  </footer>
+    `
+}
