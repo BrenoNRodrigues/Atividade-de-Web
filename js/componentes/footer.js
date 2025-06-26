c8e21c7715dfe4699bb7a8d1505cc29dfb9857f7
BrenoNRodrigues <breno.rodrigues.nascimento.silva@gmail.com>
2025-06-26 17:37:08 -0300

    Add files via upload

--- js/componentes/footer.js
+++ js/componentes/footer.js
@@ -5,14 +5,14 @@ export default function footer (){
     <div class="container">
       <div class="row">
         <div class="col s12 m6 footer-email">
-          <h4>Emails</h4> <a class="gmail" href="https://mail.google.com/mail/u/0/?hl=pt-BR#inbox"><img src="/Gmail_icon_(2020).svg.png" alt=""></a>
+          <h4>Emails</h4> <a class="gmail" href="https://mail.google.com/mail/u/0/?hl=pt-BR#inbox"><img src="/Atividade-de-Web/Gmail_icon_(2020).svg.png" alt=""></a>
           <p class="p-footer">vendas.ecotubos001@gmail.com</p>
           <p class="p-footer">vendas.ecotubos002@gmail.com</p>
           <p class="p-footer">compras.ecotubos01@gmail.com</p>
         </div>
         <div class="col s12 m6 footer-contact">
-          <h4>Contatos</h4>  <a class="whatsapp" href="https://api.whatsapp.com/send/?phone=5511966240118&text=Ol%C3%A1%2C+gostaria+de+mais+informa%C3%A7%C3%B5es+sobre+os+tubos+de+papel%C3%A3o&type=phone_number&app_absent=0"><img src="/WhatsApp.svg.webp" alt=""></a>
-    <a class="instagram" href="https://www.instagram.com/ecotubosdepapelao/"><img src="/instagram.png" alt=""></a>
+          <h4>Contatos</h4>  <a class="whatsapp" href="https://api.whatsapp.com/send/?phone=5511966240118&text=Ol%C3%A1%2C+gostaria+de+mais+informa%C3%A7%C3%B5es+sobre+os+tubos+de+papel%C3%A3o&type=phone_number&app_absent=0"><img src="/Atividade-de-Web/WhatsApp.svg.webp" alt=""></a>
+    <a class="instagram" href="https://www.instagram.com/ecotubosdepapelao/"><img src="/Atividade-de-Web/instagram.png" alt=""></a>
           <p class="p-footer">Fábio Pirozeli: (11) 9 6624-0118</p>
           <p class="p-footer">Paula: (11) 9 1640-0779</p>
           <p class="p-footer">Sheila: (11) 9 7352-9553</p>
--- js/componentes/navbar.js
+++ js/componentes/navbar.js
@@ -2,17 +2,17 @@ export default function navbar (){
     const nav = document.getElementById("navbar")
 nav.innerHTML = ` <nav class="greeneco" role="navigation">
 <div class="nav-wrapper container">
-  <a id="logo-container" href="/EcoTubosLogotipo.png" class="brand-logo"><img src="/EcoTubosLogotipo.png" alt="EcoTubosLogotipo"></a>
+  <a id="logo-container" href="/EcoTubosLogotipo.png" class="brand-logo"><img src="/Atividade-de-Web/EcoTubosLogotipo.png" alt="EcoTubosLogotipo"></a>
   <ul class="right hide-on-med-and-down">
-    <li><a href="#home">Produtos</a></li>
-    <li><a href="#servicos">Serviços</a></li>
+    <li><a href="#home">Inicios</a></li>
+    <li><a href="#servicos">Produtos</a></li>
     <li><a href="#contato">Contato</a></li>
     <li><a href="#sobrenos">Sobre Nos</a><li>
   </ul>
 
   <ul id="nav-mobile" class="sidenav">
-    <li><a href="#index">Produtos</a></li>
-    <li><a href="#servicos">Serviços</a></li>
+    <li><a href="#home">Inicio</a></li>
+    <li><a href="#servicos">Produtos</a></li>
     <li><a href="#contato">Contato</a></li>
     <li><a href="#sobrenos">Sobre Nos</a></li>
   </ul>
