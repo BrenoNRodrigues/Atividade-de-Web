061c952327a0946755ce03fa56db9d5d74a1f43f
BrenoNRodrigues <breno.rodrigues.nascimento.silva@gmail.com>
2025-06-26 16:35:34 -0300

    Add files via upload

---
+++ componentes/pageservicos.js
@@ -0,0 +1,22 @@
+export default function servicos (){ 
+    const main = document.getElementById("main")
+ main.innerHTML = `
+    <div class="container">
+    <div class="section">
+    <section class="conteudo">
+    <h2>Tubos de Papelão</h2>
+    <img src="/1000014861.jpg" class="imagem-produto" alt="Tubos empilhados">
+
+    <h3>Espessura</h3>
+    <div class="tamanhos">
+      <span>5mm / 0,5kg</span>
+      <span>10mm / 1,0kg</span>
+      <span>12mm / 1,2kg</span>
+      <span>15mm / 1,5kg</span>
+      <span>17mm / 1,7kg</span>
+      <span>20mm / 2,0kg</span>
+    </div>
+  </section>
+    </div>
+  </div>
+  ` }
